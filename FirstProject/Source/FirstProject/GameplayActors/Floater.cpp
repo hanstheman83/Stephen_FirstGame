8fe1af7334c8c2100c8e660649f6926b2f630051
hanstheman <hey2k_bad@hushmail.com>
2020-01-01 15:30:14 +0100

    video 23

---
+++ FirstProject/Source/FirstProject/GameplayActors/Floater.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Floater.h"
+#include "Components/StaticMeshComponent.h"
+
+
+// Sets default values
+AFloater::AFloater()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CustomStaticMesh"));
+
+	InitialLocation = FVector(0.0f);
+	PlacedLocation = FVector(0.0f);
+	WorldOrigin = FVector(.0f, .0f, .0f);
+
+	InitialDirection = FVector(.0f, .0f, .0f);
+
+	bInitializeFloaterLocations = false;
+	bShouldFloat = false;
+
+	InitialForce = FVector(200000.f, 0.f, 0.f);
+	InitialTorque = FVector(200000.f, 0.f, 0.f);
+
+	RunningTime = 0.f;
+
+	Amplitude = 1.f;
+	TimeStretch = 1.f;
+
+}
+
+// Called when the game starts or when spawned
+void AFloater::BeginPlay()
+{
+	Super::BeginPlay();
+
+	float InitialX = FMath::FRandRange(-500.f,500.f);
+	float InitialY = FMath::FRandRange(-500.f, 500.f);
+	float InitialZ = FMath::FRandRange(0.f, 500.f);
+
+	InitialLocation.X = InitialX;
+	InitialLocation.Y = InitialY;
+	InitialLocation.Z = InitialZ;
+
+
+
+
+	PlacedLocation = GetActorLocation();
+
+	if (bInitializeFloaterLocations)
+	{
+		SetActorLocation(InitialLocation);
+	}
+
+	StaticMesh->AddForce(InitialForce);
+	StaticMesh->AddTorque(InitialTorque);
+	
+
+}
+
+// Called every frame
+void AFloater::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bShouldFloat)
+	{
+		FVector NewLocation = GetActorLocation();
+
+		NewLocation.X = NewLocation.X + Amplitude * FMath::Sin(TimeStretch * RunningTime);
+		NewLocation.Y = NewLocation.Y + Amplitude * FMath::Cos(TimeStretch * RunningTime);
+		NewLocation.Z = NewLocation.Z + Amplitude * FMath::Sin(TimeStretch * RunningTime);
+
+		SetActorLocation(NewLocation);
+
+		RunningTime += DeltaTime;
+	}
+
+}
+
