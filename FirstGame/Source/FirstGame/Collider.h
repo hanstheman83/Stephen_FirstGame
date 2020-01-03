c276f934b6bbcfec3858c112ff188ac76ed0adf0
hanstheman <hey2k_bad@hushmail.com>
2020-01-03 21:16:57 +0100

    Video 27

---
+++ FirstGame/Source/FirstGame/Collider.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Collider.generated.h"
+
+UCLASS()
+class FIRSTGAME_API ACollider : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACollider();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class USphereComponent* SphereComponent;
+
+	FORCEINLINE UStaticMeshComponent* GetMeshComponent() { return MeshComponent; } // FORCEINLINE with return declaration in header is FASTER!
+	FORCEINLINE void SetMeshComponent(UStaticMeshComponent* Mesh) { MeshComponent = Mesh; }
+	
+	FORCEINLINE USphereComponent* GetSphereComponent() { return SphereComponent; } // FORCEINLINE with return declaration in header is FASTER!
+	FORCEINLINE void SetSphereComponent(USphereComponent* Sphere) { SphereComponent = Sphere; }
+};
