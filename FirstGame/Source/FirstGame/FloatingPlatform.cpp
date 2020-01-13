1a829dc3e1084e16c935228a34d3387c63f014bf
hanstheman <hey2k_bad@hushmail.com>
2020-01-13 23:56:31 +0100

    Video 47 - Floating Platform

---
+++ FirstGame/Source/FirstGame/FloatingPlatform.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloatingPlatform.h"
+#include "Components/StaticMeshComponent.h"
+#include "TimerManager.h"
+
+// Sets default values
+AFloatingPlatform::AFloatingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	StartPoint = FVector(0.f);
+	EndPoint = FVector(0.f);
+
+	bInterping = false;
+
+
+	InterpSpeed = 1.f;
+	InterpTime = 1.f;
+
+}
+
+// Called when the game starts or when spawned
+void AFloatingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartPoint = GetActorLocation();
+	EndPoint += StartPoint;
+
+	bInterping = false;
+
+	GetWorldTimerManager().SetTimer(InterpTimer, this, &AFloatingPlatform::ToggleInterping, InterpTime); // calling ToggleInterping()
+	
+	Distance = (EndPoint - StartPoint).Size(); // .Size() will get magnitude of returned vector
+}
+
+// Called every frame
+void AFloatingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bInterping)
+	{
+		FVector CurrentLocation = GetActorLocation();
+		FVector Interp = FMath::VInterpTo(CurrentLocation, EndPoint, DeltaTime, InterpSpeed);
+		SetActorLocation(Interp);
+
+		float DistanceTraveled = (GetActorLocation() - StartPoint).Size(); //.Size() will always return a positive value!
+		if ( FMath::IsNearlyZero( (Distance - DistanceTraveled), 1.f ) )
+		{
+			ToggleInterping();
+
+			GetWorldTimerManager().SetTimer(InterpTimer, this, &AFloatingPlatform::ToggleInterping, InterpTime);
+			SwapVectors(StartPoint, EndPoint);
+		}
+	}
+}
+
+void AFloatingPlatform::ToggleInterping()
+{
+	bInterping = !bInterping;
+}
+
+void AFloatingPlatform::SwapVectors(FVector& VecOne, FVector& VecTwo)
+{
+	FVector Temp = VecOne;
+	VecOne = VecTwo;
+	VecTwo = Temp;
+}
+
+
+
